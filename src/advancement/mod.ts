77e0a56ded3e6ecb9b342156af1e8368cbc1e7c8
Antonin Huaut <antonin.huaut@gmail.com>
2022-04-20 14:42:09 +0200

    move

--- src/advancement/mod.ts
+++ src/advancement/mod.ts
@@ -1,11 +1,23 @@
-import {colors, colorsMapping, getAdvancementsPathType, types, writeFile} from "../utils/pack.ts"
+import {
+    colors,
+    colorsMapping,
+    getAdvancementsPath,
+    getAdvancementsPathBodyColor,
+    getAdvancementsPathType,
+    getDatapackName,
+    types,
+    writeFile
+} from "../utils/pack.ts"
 import {calculateModelData, getVariantsWithTypeColor} from "../utils/variant.ts"
-import generateMainFile from "./mainFile.ts"
-import generatePatternFiles from "./patternFile.ts"
-import generateActiveFile from "./activeFile.ts"
-import generateGlobalFile from "./globalFile.ts"
-import {getParentTemplate_mod} from "./advancementFactory.ts";
-import {Variant} from "./IJson.ts";
+import {
+    getActiveTemplate,
+    getMainTemplate_GlobalFile,
+    getMainTemplate_MainFile,
+    getParentRewardsTemplate,
+    getParentTemplate_globalFile,
+    getParentTemplate_mod
+} from "./advancementFactory.ts";
+import {Criteria, Variant} from "./IJson.ts";
 
 export default async function generatesFiles() {
     const promises: Promise<void>[] = []
@@ -63,5 +75,101 @@ export default async function generatesFiles() {
     })
 
     promises.push(generateGlobalFile(allTypeVariants))
+    await Promise.all(promises)
+}
+
+async function generateMainFile(type: string, typesVariants: {
+    key: string, value: Variant
+}[]) {
+    const path = `${getAdvancementsPathType(type)}/main.json`
+    const content = getMainTemplate_MainFile({
+        modelData: calculateModelData(types.indexOf(type), 0, 7),
+        type: type
+    })
+
+    for (const variant of typesVariants) {
+        content.criteria[variant.key] = variant.value
+    }
+
+    await writeFile(path, content)
+}
+
+async function generateActiveFile(type: string, colorBody: string) {
+    const colorPattern = "yellow"
+
+    const path = `${getAdvancementsPathBodyColor(type, colorBody)}/active.json`
+    const content = getActiveTemplate({
+        type: type,
+        colorBody: colorBody,
+        colorPattern: colorPattern
+    })
+    await writeFile(path, content)
+}
+
+async function generatePatternFiles(params: {
+    type: string,
+    colorBody: string,
+    colorPattern: string,
+    variantObj: { key: string, value: Variant }
+}) {
+    const criteriaItem: Criteria = {
+        [params.variantObj.key]: params.variantObj.value
+    }
+
+    const colorPatternIndex = colors.indexOf(params.colorPattern)
+    let parent = params.type
+    if (colorPatternIndex == 0) {
+        parent = `${parent}/body_${params.colorBody}`
+    } else {
+        const previousColor = colors[colorPatternIndex - 1]
+        parent = `${parent}/${params.colorBody}/pattern_${previousColor}`
+    }
+
+    const content = getParentRewardsTemplate({
+        bodyColor: params.colorBody,
+        modelData: calculateModelData(types.indexOf(params.type), colors.indexOf(params.colorBody), colorPatternIndex),
+        parent: parent,
+        patternColor: params.colorPattern,
+        type: params.type
+    })
+
+    content.criteria = criteriaItem
+
+    const path = `${getAdvancementsPathBodyColor(params.type, params.colorBody)}/pattern_${params.colorPattern}.json`
+    await writeFile(path, content)
+}
+
+async function generateGlobalFile(allTypesVariants: {
+    [type: string]: { key: string, value: Variant }[]
+}) {
+    const promises: Promise<void>[] = []
+
+    const path = `${getAdvancementsPath()}/global.json`
+    const content = getMainTemplate_GlobalFile()
+    let lastParent = "global"
+
+    for (const type of Object.keys(allTypesVariants)) {
+        const typePath = `${getAdvancementsPath()}/global_${type}.json`
+        const typeContent = getParentTemplate_globalFile({
+            modelData: calculateModelData(types.indexOf(type), 0, 7),
+            parent: lastParent,
+            type: type
+        })
+
+        for (const variant of allTypesVariants[type]) {
+            content.criteria[variant.key] = variant.value
+            typeContent.criteria[variant.key] = variant.value
+        }
+
+        promises.push(writeFile(typePath, typeContent))
+
+        lastParent = `global_${type}`
+    }
+
+    promises.push(writeFile(`${getAdvancementsPath()}/global_tick.json`, {
+        "criteria": {"active": {"trigger": "minecraft:tick"}},
+        "parent": `${getDatapackName()}:${lastParent}`
+    }))
+    promises.push(writeFile(path, content))
     await Promise.all(promises)
 }
